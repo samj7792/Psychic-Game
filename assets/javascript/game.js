77731f673af2c8a56c0b061cb62834d6059486e3
Sam Jackson <samuelsjackson7@gmail.com>
2019-06-13 15:30:41 -0400

    linked game.js and jQuery in index, per js: computer guesses random letter, if user guess matches score goes up, otherwise lives go down.

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+//Creates an array that lists out all options for the computer to choose from.
+var compChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// Randomly Chooses a choice from the compChoices array. This is the Computer's guess.
+var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)]; 
+
+console.log(compGuess)
+
+var wins = 1;
+
+var losses = 1;
+
+var lives = 8;
+
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+
+    // Determines which key was pressed by user.
+    var userGuess = event.key;
+
+    console.log(userGuess);
+
+    // var userGuessLower = userGuess.toLowerCase;
+
+    if (userGuess === compGuess) {
+        document.getElementById("winCount").innerHTML = wins++;
+    }
+
+    else {
+        document.getElementById("guessCount").innerHTML = lives--;
+    }
+
+}
+
